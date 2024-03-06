55c42b0ec9d21096202e92afc4c33df6e043b252
titouanck <chevrier.titouan@gmail.com>
2024-03-06 17:36:54 +0100

    models

--- srcs/pong/admin.py
+++ srcs/pong/admin.py
@@ -1,3 +1,9 @@
+from .models import Player, Tournament, Tournament_Player, Match, Relation
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Player)
+admin.site.register(Tournament)
+admin.site.register(Tournament_Player)
+admin.site.register(Match)
+admin.site.register(Relation)
