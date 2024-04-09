e48fbed24a5fabe9c51c3483174afd0f5227200b
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2024-04-09 16:54:51 +0200

    ratio canvns

--- srcs/pong/static/js/backGround.js
+++ srcs/pong/static/js/backGround.js
@@ -1,7 +1,7 @@
 particlesJS('particles-js', {
 	"particles": {
 	  "number": {
-		"value": 80,
+		"value": 10,
 		"density": {
 		  "enable": true,
 		  "value_area": 800
@@ -26,11 +26,11 @@ particlesJS('particles-js', {
 		}
 	  },
 	  "opacity": {
-		"value": 0.5,
+		"value": 1,
 		"random": false,
 		"anim": {
 		  "enable": false,
-		  "speed": 1,
+		  "speed": 0.5,
 		  "opacity_min": 0.1,
 		  "sync": false
 		}
@@ -47,7 +47,7 @@ particlesJS('particles-js', {
 	  },
 	  "line_linked": {
 		"enable": true,
-		"distance": 70,
+		"distance": 100,
 		"color": "#ffffff",
 		"opacity": 0.4,
 		"width": 1
@@ -58,8 +58,8 @@ particlesJS('particles-js', {
 		"direction": "none",
 		"random": false,
 		"straight": false,
-		"out_mode": "out",
-		"bounce": false,
+		"out_mode": "bounce",
+		"bounce": true,
 		"attract": {
 		  "enable": false,
 		  "rotateX": 600,
